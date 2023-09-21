f32bcfa5989d5bf037202295c21db709a75be0cc
Pirisinu <palazzialessandro2@gmail.com>
2023-09-21 14:38:38 +0200

    [ADD]Array di persone

--- README.md
+++ README.md
@@ -23,3 +23,25 @@ Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.*
 1. Barbara Ramos
     - Work -->	Graphic Designer
     -	IMG --> barbara-ramos-graphic-designer.jpg
+----------------------------------------------------------------
+## Svolgimento:
+
+1. Creare un array di oggetti 
+1. Ogni oggetto sara una 'persona' con seguenti caratteristiche_
+    - Nome
+    - Lavoro
+    - La sua foto
+1. 
+1. 
+1. 
+
+
+## Risultato finale:
+Nella pagina Html ci sara un:
+- H1('Our Team')
+- p('Lorem ipsum')
+- un box con:
+  - 6 card contenenti:
+  1. La sua foto
+  1. Nome
+  1. Lavoro
---
+++ js/script.js
@@ -0,0 +1,33 @@
+const persone = [
+  {
+    nome: 'Wayne Barnett',
+    lavoro: 'Founder & CEO',
+    imgPersonale: 'wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    nome: 'Angela Caroll',
+    lavoro: 'Chief Editor',
+    imgPersonale: 'angela-caroll-chief-editor.jpg'
+  },
+  {
+    nome: 'Walter Gordon',
+    lavoro: 'Office Manager',
+    imgPersonale: 'walter-gordon-office-manager.jpg'
+  },
+  {
+    nome: 'Angela Lopez',
+    lavoro: 'Social Media Manager',
+    imgPersonale: 'angela-lopez-social-media-manager.jpg'
+  },
+  {
+    nome: 'Scott Estrada',
+    lavoro: 'Developer',
+    imgPersonale: 'scott-estrada-developer.jpg'
+  },
+  {
+    nome: 'Barbara Ramos',
+    lavoro: 'Graphic Designer',
+    imgPersonale: 'barbara-ramos-graphic-designer.jpg'
+  }
+];
+console.log(persone)
